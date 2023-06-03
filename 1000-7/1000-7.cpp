6493faa7ec8180b53a5ac6234a304b3859922e35
1ando11 <you@example.com>
2023-06-03 12:39:38 +0200

    Add project files.

---
+++ 1000-7/1000-7.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <thread>
+using namespace std;
+
+
+int main()
+{
+	for (int i = 1000; i > 6; i -= 7)
+	{
+		cout << i << "-7" << endl;
+		this_thread::sleep_for(chrono::milliseconds(60));
+	}
+}
